0d872ce05de2255f05d075660a4e7429138a7da8
Rithvik Rao <rithvik1705@gmail.com>
2018-06-06 11:38:31 +0530

    Portfolio added using resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_070957) do
+ActiveRecord::Schema.define(version: 2018_06_06_060550) do
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "title"
